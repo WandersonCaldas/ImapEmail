62e1f0367b17027a1fdd9f1d438ef1e08e03333b
Wanderson Caldas <wcaldasti@gmail.com>
2020-10-26 11:15:16 -0300

    CRIAR PROJETO

---
+++ eGmail/src/Program.cs
@@ -0,0 +1,32 @@
+﻿using Limilabs.Client.IMAP;
+using Limilabs.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace src
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Imap imap = new Imap())
+            {
+                imap.Connect("imap.gmail.com", 993, true);
+                imap.Login("user", "password");                
+                imap.SelectInbox();
+                List<long> uids = imap.Search(Flag.All);
+                foreach (long uid in uids)
+                {
+                    IMail email = new MailBuilder()
+                        .CreateFromEml(imap.GetMessageByUID(uid));
+                    Console.WriteLine(email.Subject);
+                }
+                imap.Close();
+            }
+
+        }
+    }
+}
